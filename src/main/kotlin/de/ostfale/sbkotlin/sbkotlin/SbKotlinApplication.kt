05acbb8df79666824e454639dfda1b7b5e46c4ad
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2019-08-15 18:49:22 +0200

    Initial commit

---
+++ src/main/kotlin/de/ostfale/sbkotlin/sbkotlin/SbKotlinApplication.kt
@@ -0,0 +1,11 @@
+package de.ostfale.sbkotlin.sbkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SbKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/de/ostfale/sbkotlin/sbkotlin/SbKotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.ostfale.sbkotlin.sbkotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SbKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
